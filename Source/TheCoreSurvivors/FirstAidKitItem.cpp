41f56b3c58f2b401a1c3c04cd48a8213ccf4ba8a
Raulmind <rmingallon@gmail.com>
2023-04-19 18:37:48 +0200

    Clases de item y sus subclases, prefabs y materiales para los dos tipos de items

---
+++ Source/TheCoreSurvivors/FirstAidKitItem.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FirstAidKitItem.h"
+
+// Sets default values
+AFirstAidKitItem::AFirstAidKitItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFirstAidKitItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFirstAidKitItem::Init(float newValue)
+{
+	lifeRecovered = newValue;
+}
+
+// Called every frame
+void AFirstAidKitItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
